12e7e10e36b3038954e52d387f236c3a6dadaaab
Pete Rossman <Pete Rossman>
2017-03-13 12:42:13 -0400

    First working version of DateDiff program.

--- DateDiff/Program.cs
+++ DateDiff/Program.cs
@@ -10,17 +10,56 @@ namespace DateDiff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a date in MM/DD/YYYY format:");
+            Console.WriteLine("Enter the earlier of the two dates in MM/DD/YYYY format:");
             string date1 = Console.ReadLine();
-            Console.WriteLine("Enter a second date in MM/DD/YYYY format:");
+            Console.WriteLine("Enter the later of the two dates in MM/DD/YYYY format:");
             string date2 = Console.ReadLine();
             DateTime dt1;
             DateTime dt2;
             DateTime.TryParse(date1, out dt1);
             DateTime.TryParse(date2, out dt2);
-            Console.Write(dt1);
-            Console.WriteLine("\n");
-            Console.Write(dt2);
+            int month1 = dt1.Month;
+            int month2 = dt2.Month;
+            int year1 = dt1.Year;
+            int year2 = dt2.Year;
+            int day1 = dt1.Day;
+            int day2 = dt2.Day;
+            int yearDiff = year2 - year1;
+            int monthDiff = month2 - month1;
+            int dayDiff = day2 - day1;
+
+            if (month2 < month1)
+                {
+                    yearDiff--;
+                }
+            if (month1 == month2 && day2 < day1)
+                {
+                    yearDiff--;
+                    monthDiff = 11;
+                }
+            if (monthDiff < 0)
+                {
+                    monthDiff = monthDiff + 12;
+                }
+            if (day2 < day1)
+                {
+                    if (month1 == 2)
+                    {
+                        dayDiff = day2 + (28 - day1);
+                    }
+                    else if (month1 == 4 || month1 == 6 || month1 == 9 || month1 == 11)
+                    {
+                        dayDiff = day2 + (30 - day1);
+                    }
+                    else
+                    {
+                        dayDiff = day2 + (31 - day1);
+                    }
+                }
+
+            Console.WriteLine("The Year difference is: {0}", yearDiff);
+            Console.WriteLine("The Month difference is: {0}", monthDiff);
+            Console.WriteLine("The Day difference is: {0}", dayDiff);
             Console.ReadLine();
         }
     }
